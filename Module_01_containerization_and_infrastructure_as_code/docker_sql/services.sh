6d9fbe9df521ffd3268bb3cb8ef852442ebff391
Ahmad Ryan Permana <ahmadryanpermana@gmail.com>
2025-01-21 00:08:01 +0700

    edit services.sh

--- Module_01_containerization_and_infrastructure_as_code/docker_sql/services.sh
+++ Module_01_containerization_and_infrastructure_as_code/docker_sql/services.sh
@@ -1,7 +1,7 @@
 docker run -it \
-    -e POSTGRES_USER="root" \
-    -e POSTGRES_PASSWORD="root" \
+    -e POSTGRES_USER="Postgres" \
+    -e POSTGRES_PASSWORD="Postgres" \
     -e POSTGRES_DB="ny_taxi" \
     -v c:/Users/gamers/data-engineering-zoomcamp/Module_01_containerization_and_infrastructure_as_code/docker_sql/ny_taxi_postgres_data:/var/lib/postgresql/data \
-    -p 5432:5432 \
+    -p 5431:5432 \
     postgres:13 
